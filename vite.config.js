262a73d7ee41ddd33cc5ee97d3946467f67627b7
machineke <markarapsoi72@gmail.com>
2025-06-05 17:38:09 +0300

    remove copy command for routes config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-staticwebapp-config',
-      writeBundle() {
-        const src = resolve(__dirname, 'staticwebapp.config.json');
-        const dest = resolve(__dirname, 'dist', 'staticwebapp.config.json');
-        copyFileSync(src, dest);
-        console.log(`Copied ${src} to ${dest}`);
-      }
-    }
   ],
   build: {
     outDir: 'dist'
