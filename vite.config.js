a6f5cf4bf4292b35a60cde11389deb4c6cf612fb
machineke <markarapsoi72@gmail.com>
2025-06-08 19:37:53 +0300

    fix the error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  build: {
-    outDir: 'dist'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: '/',
+    plugins: [
+      react(),
+    ],
+    build: {
+      outDir: 'dist'
+    }
   }
 })
